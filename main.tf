57a923f6ce18490f1db399e118985e999f647464
Lahiru Fernando <lahirushanaka@gmail.com>
2022-07-24 13:19:14 +0530

    testing

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ provider "aws" {
 module "vpc" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git"
   name = "${var.environment}-vpc"
-  cidr = lookup(var.network_cidr,var.environment, null)
+  cidr = var.network_cidr
   azs   = ["${var.region}a","${var.region}b"]
-  private_subnets = lookup(var.private_subnet_cidrs,var.environment, null)
-  public_subnets  = lookup(var.public_subnet_cidrs,var.environment, null)
+  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnet_cidrs
   enable_nat_gateway = true
 }
