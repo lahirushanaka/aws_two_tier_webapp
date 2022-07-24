52af58d168eb37a4b8ffb694fa1ce3079240601f
Lahiru Fernando <lahirushanaka@gmail.com>
2022-07-24 12:46:16 +0530

    testing

--- variable.tf
+++ variable.tf
@@ -5,16 +5,16 @@ variable "environment" {
 }
 
 variable "network_cidr" {
-  type    = list(object({
-      dev = string
-      uat = string
-      production = string
-  }))
-  default = {
-    dev    = "10.0.0.0/24"
-    uat        = "10.0.1.0/24"
-    production = "10.0.2.0/24"
-  }
+#   type    = list(object({
+#       dev = string
+#       uat = string
+#       production = string
+#   }))
+#   default = {
+#     dev    = "10.0.0.0/24"
+#     uat        = "10.0.1.0/24"
+#     production = "10.0.2.0/24"
+#   }
 }
 
 variable "region" {
@@ -23,28 +23,28 @@ variable "region" {
 }
 
 variable "private_subnet_cidrs" {
-  type    = list(object({
-      dev = string
-      uat = string
-      production = string
-  }))
-  default = {
-    dev    = ["10.0.0.0/26", "10.0.0.64/26"]
-    uat         = ["10.0.1.0/26", "10.0.1.64/26"]
-    production = ["10.0.2.0/26", "10.0.2.64/26"]
-  }
+#   type    = list(object({
+#       dev = string
+#       uat = string
+#       production = string
+#   }))
+#   default = {
+#     dev    = ["10.0.0.0/26", "10.0.0.64/26"]
+#     uat         = ["10.0.1.0/26", "10.0.1.64/26"]
+#     production = ["10.0.2.0/26", "10.0.2.64/26"]
+#   }
 }
 
 variable "public_subnet_cidrs" {
-  type    = list(object({
-      dev = string
-      uat = string
-      production = string
-  }))
-  default = {
-    dev    = ["10.0.0.128/26", "10.0.0.192/26"]
-    uat         = ["10.0.1.128/26", "10.0.1.192/26"]
-    production = ["10.0.2.128/26", "10.0.2.192/26"]
-  }
+#   type    = list(object({
+#       dev = string
+#       uat = string
+#       production = string
+#   }))
+#   default = {
+#     dev    = ["10.0.0.128/26", "10.0.0.192/26"]
+#     uat         = ["10.0.1.128/26", "10.0.1.192/26"]
+#     production = ["10.0.2.128/26", "10.0.2.192/26"]
+#   }
 }
 
